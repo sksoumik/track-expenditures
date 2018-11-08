cc578131ea0805daef7d7ae86bdda71cb51c11d2
sksoumik <sadmanks@gmail.com>
2018-11-09 03:40:59 +0600

    model

--- budget/admin.py
+++ budget/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Project, Expense, Category
 
-# Register your models here.
+admin.site.register(Project)
+admin.site.register(Expense)
+admin.site.register(Category)
